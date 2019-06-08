9f56380d6f5f89b42717601661acd413552252fe
Erick Roy Fernandez Terrel <33358659+Roy360erick@users.noreply.github.com>
2019-06-07 21:36:10 -0500

    Add files via upload
    add data

---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO `xamarin`.`incidencias` (`id`, `acusado`, `estado`, `fecha_incidencia`, `motivo`, `tipo_inicidencia`) VALUES ('1', 'Erick Fernandez', 0 , '2019-10-10', 'no sabria decirte', '1');
+INSERT INTO `xamarin`.`incidencias` (`id`, `acusado`, `estado`, `fecha_incidencia`, `motivo`, `tipo_inicidencia`) VALUES ('2', 'Brayan Castro', 0 , '2019-10-10', 'no sabria decirte', '1');
+INSERT INTO `xamarin`.`incidencias` (`id`, `acusado`, `estado`, `fecha_incidencia`, `motivo`, `tipo_inicidencia`) VALUES ('3', 'Anibal Suarez', 1 , '2019-10-10', 'no sabria decirte', '1');
+INSERT INTO `xamarin`.`incidencias` (`id`, `acusado`, `estado`, `fecha_incidencia`, `motivo`, `tipo_inicidencia`) VALUES ('4', 'Claudio Herrera', 1 , '2019-10-10', 'no sabria decirte', '1');
+INSERT INTO `xamarin`.`incidencias` (`id`, `acusado`, `estado`, `fecha_incidencia`, `motivo`, `tipo_inicidencia`) VALUES ('5', 'Edwin Huisa', 1 , '2019-10-10', 'no sabria decirte', '1');
+INSERT INTO `xamarin`.`incidencias` (`id`, `acusado`, `estado`, `fecha_incidencia`, `motivo`, `tipo_inicidencia`) VALUES ('6', 'Pedro Pinto', 0 , '2019-10-10', 'no sabria decirte', '1');
+
+
